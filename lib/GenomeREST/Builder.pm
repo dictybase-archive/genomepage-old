3a39461f30ee365cbb39b3abaebe1650d2bd9689
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2011-03-28 17:19:16 -0500

    Added oracle ddl in shared folder Added Module::Build::Chado dependencies Added basic database handling actions for the GenomeREST::Builder mostly for handling legacy schema

--- lib/GenomeREST/Builder.pm
+++ lib/GenomeREST/Builder.pm
@@ -1,107 +1,110 @@
 package GenomeREST::Builder;
 use File::Spec::Functions;
 use Carp;
-use Archive::Extract;
-use File::Path;
-use Path::Class;
 use Try::Tiny;
-use TAP::Harness;
-use base qw/Module::Build/;
+use DBI;
+use Class::MOP;
+use Storable qw/dclone/;
+use base qw/Module::Build::Chado/;
 
-sub load_core_fixture {
-    my ($self) = @_;
 
-    require dicty::Tests::Data;
-    my $data = dicty::Tests::Data->new();
-    $self->notes( loader => $data );
+__PACKAGE__->property('legacy_ddl' ,  default => sub {
+	return catfile(__PACKAGE__->current->base_dir,  'share',  'ddl',
+	'multigenome_legacy.oracle');	
+});
 
-    if ( !$data->check_chromosome() ) {
-        try {
-            $data->insert_core_data();
-        }
-        catch {
-            die "unable to insert data: $_";
-        }
+__PACKAGE__->property('ddl' ,  default => sub {
+	return catfile(__PACKAGE__->current->base_dir,  'share',  'ddl',
+	'multigenome_chado.oracle');	
+});
+
+__PACKAGE__->property('user' => 'TEST_CHADO');
+__PACKAGE__->property('password' => 'TEST_CHADO');
+
+__PACKAGE__->property('legacy_dsn' => 'dbi:Oracle:host=192.168.60.10;sid=dictybase');
+__PACKAGE__->property('legacy_user' => 'TEST_LEGACY');
+__PACKAGE__->property('legacy_password' => 'TEST_LEGACY');
+__PACKAGE__->property('_legacy_handler');
+
+
+sub legacy_setup {
+	my ($self) = @_;
+    print "running legacy setup\n" if $self->args('test_debug');
+    
+    return if $self->config('legacy_setup_done');
+
+    my ( $scheme, $driver, $attr_str, $attr_hash, $driver_dsn )
+        = DBI->parse_dsn( $self->legacy_dsn )
+        or croak "cannot parse dbi dsn";
+
+	my $db_class = 'Module::Build::Chado::' . ucfirst lc $driver;
+    Class::MOP::load_class($db_class);
+    my $legacy = $db_class->new( module_builder => $self );
+    for my $attr (qw/ddl dsn user password/) {
+    	my $api = 'legacy_'.$attr;
+    	$legacy->$attr($self->$api);
     }
+    $self->_legacy_handler($legacy);
+    $self->config( 'legacy_setup_done', 1 );
+    print "done with legacy setup\n" if $self->args('test_debug');
+
 }
 
-sub load_fixture {
+sub ACTION_deploy_legacy_schema {
+	my ($self) = @_;
+	$self->setup_legacy;
+    $self->config( 'is_legacy_db_created', 1 );
+	if (!$self->config('is_legacy_schema_loaded')) {
+		$self->_legacy_handler->deploy_schema;
+		$self->config('is_legacy_schema_loaded');
+        print "loaded legacy schema\n" if $self->args('test_debug');
+	}
+}
+
+sub ACTION_deploy_schema {
+	my ($self) = @_;	
+    $self->SUPER::ACTION_deploy_schema(@_);
+    $self->depends_on('deploy_legacy_schema');
+}
+
+
+sub ACTION_load_fixture {
+	my ($self) = @_;
+    $self->depends_on('deploy_schema');
+    $self->SUPER::ACTION_load_fixture(@_);
+
+    ## -- now load additional data
+
+    ## -- then load fixtures for legacy schema
+}
+
+sub ACTION_unload_fixture {
+	my ($self) = @_;
+    $self->SUPER::ACTION_unload_fixture(@_);
+
+    ## -- unload anything additional
+
+    ## -- unload legacy fixture
+    $self->depends_on('legacy_setup');
+}
+
+sub ACTION_prune_fixture {
+	my ($self) = @_;
+    $self->SUPER::ACTION_prune_fixture(@_);
+    $self->depends_on('legacy_setup');
+    $self->_legacy_handler->prune_fixture;
+    $self->config( 'is_legacy_fixture_loaded',   0 );
+    $self->config( 'is_legacy_fixture_unloaded', 1 );
+}
+
+
+sub ACTION_drop_schema {
     my ($self) = @_;
-
-    require dicty::Tests::Data;
-    my $data = dicty::Tests::Data->new();
-    $self->notes( loader => $data );
-
-    if ( !$data->check_chromosome() ) {
-        try {
-            $data->insert_whole_chromosome();
-        }
-        catch {
-            die "unable to insert data: $_";
-        }
-    }
+    $self->SUPER::ACTION_drop_schema(@_);
+    $self->depends_on('legacy_setup');
+    $self->_legacy_handler->drop_schema;
+    $self->config_data( 'is_legacy_schema_loaded' => 0 );
 }
 
-sub unload_fixture {
-    my ($self) = @_;
-    my $loader = $self->notes('loader');
-    try {
-        $loader->unload_data();
-    }
-    catch {
-        die "error in unloading: $_";
-    }
-}
-
-sub ACTION_deploy {
-    my ($self) = @_;
-    $self->depends_on('dist');
-    my $file = catfile( $self->base_dir, $self->dist_dir . '.tar.gz' );
-    my $archive = Archive::Extract->new( archive => $file );
-    my $path = $self->prompt( 'Extract archive to:', $ENV{HOME} );
-    my $fullpath = catdir( $path, $self->dist_dir );
-    if ( -e $fullpath ) {
-        rmtree( $fullpath, { verbose => 1 } );
-    }
-    $archive->extract( to => $path ) or confess $archive->error;
-    my $logpath        = catdir( $fullpath, 'log' );
-    my $cache_path     = catdir( $fullpath, 'tmp' );
-    my $web_cache_path = catdir( $fullpath, 'webtmp' );
-
-    mkpath( $logpath, { verbose => 1, mode => 0777 } );
-    chmod 0777, $logpath;
-
-    mkpath( $cache_path, { verbose => 1, mode => 0777 } );
-    chmod 0777, $cache_path;
-
-    mkpath( $web_cache_path, { verbose => 1, mode => 0774 } );
-    chmod 0777, $web_cache_path;
-
-    #now make the conf files readable
-    my @conf = map { $_->stringify } dir( $fullpath, 'conf' )->children();
-    chmod 0644, $_ foreach @conf;
-
-}
-
-#run tests files without the string 'optional' in their name
-sub ACTION_testcore {
-    my ($self) = @_;
-    $self->load_core_fixture;
-
-    $self->depends_on('build');
-    my $tap = TAP::Harness->new(
-        { lib => [ catdir( $self->base_dir, 'blib', 'lib' ) ] } );
-    my @test_files = grep { !/optional/ } @{ $self->find_test_files };
-
-    $tap->runtests(@test_files);
-    $self->unload_fixture;
-}
-
-sub ACTION_test {
-    my ( $self, @arg ) = @_;
-    $self->load_core_fixture();
-    $self->SUPER::ACTION_test(@arg);
-    $self->unload_fixture;
-}
 
 1;
