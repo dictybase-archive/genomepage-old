f919a3566e2f164251b6407cf0820b4f13e1dc32
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2011-11-19 15:56:32 -0600

    Controller for handling protein feature

---
+++ lib/GenomeREST/Controller/Protein.pm
@@ -0,0 +1,59 @@
+package GenomeREST::Controller::Protein;
+
+use strict;
+use Genome::Tabview::Page::Gene;
+use Genome::Factory::Tabview::Tab;
+use Genome::Factory::Tabview::Section;
+use base 'GenomeREST::Controller';
+
+sub show_tab_html {
+    my ($self) = @_;
+    my $db = Genome::Tabview::Page::Gene->new(
+        primary_id => $self->stash('gene_id'),
+        active_tab => 'protein',
+        context    => $self,
+        model      => $self->app->modware->handler
+    );
+    $self->stash( $db->result );
+    $self->render( template => 'protein/index' );
+
+}
+
+sub show_tab_json {
+    my ($self) = @_;
+    my $factory = Genome::Factory::Tabview::Tab->new(
+        tab        => 'protein',
+        primary_id => $self->stash('gene_id'),
+        model      => $self->app->modware->handler,
+        context    => $self
+    );
+    $self->render_json( $self->panel_to_json($factory) );
+}
+
+sub show_section_json {
+    my ($self) = @_;
+    my $factory;
+    $factory = Genome::Factory::Tabview::Section->new(
+        primary_id => $self->stash('gene_id'),
+        tab        => 'protein',
+        section    => $self->stash('id'),
+        context    => $self,
+        model      => $self->app->modware->handler
+    );
+    $self->render_json( $self->panel_to_json($factory) );
+}
+
+sub show_subsection_json {
+	my ($self) = @_;
+    my $factory = Genome::Factory::Tabview::Section->new(
+        primary_id => $self->stash('gene_id'),
+        tab        => $self->stash('tab'),
+        section    => 'protein',
+        context    => $self,
+        model      => $self->app->modware->handler
+        sub_section => $self->stash('sub_section')
+    );
+    $self->render_json( $self->panel_to_json($factory) );
+}
+
+1;
