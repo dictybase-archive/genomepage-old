2153c6dd6d093fafa2753250f69c61b79aae5d45
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2011-11-19 18:17:19 -0600

    Removed section_html method Removed sub_section method Kept only html json forming methods for tab and section

--- lib/GenomeREST/Controller/Gene.pm
+++ lib/GenomeREST/Controller/Gene.pm
@@ -1,10 +1,9 @@
 package GenomeREST::Controller::Gene;
 
 use strict;
-
-#use Genome::Tabview::Page::Gene;
-#use Genome::Factory::Tabview::Tab;
-#use Genome::Factory::Tabview::Section;
+use Genome::Tabview::Page::Gene;
+use Genome::Factory::Tabview::Tab;
+use Genome::Factory::Tabview::Section;
 use Module::Load;
 use Try::Tiny;
 use base 'GenomeREST::Controller';
@@ -66,51 +65,7 @@ sub show {
     }
     $self->set_organism($common_name);
 
-    if ( $self->stash('format') and $self->stash('format') eq 'json' ) {
-        return $self->show_json;
-    }
-    $self->show_html;
-}
-
-sub show_html {
-    my ($self)  = @_;
-    my $gene_id = $self->stash('gene_id');
-    my $tabview = Genome::Tabview::Page::Gene->new(
-        primary_id => $gene_id,
-        base_url   => $self->url_for('current'),
-        active_tab => 'gene'
-    );
-    $tabview->model( $self->app->modware->handler );
-    $self->stash( $tabview->result );
-    $self->render( template => 'gene/index' );
-}
-
-sub show_json {
-    my ($self) = @_;
-    my $gene_id = $self->stash('gene_id');
-
-    my $factory = Genome::Factory::Tabview::Tab->new(
-        primary_id => $gene_id,
-        tab        => 'gene',
-        context    => $self
-    );
-
-    my $tabview = $factory->instantiate;
-    $tabview->model( $self->app->modware->handler );
-    $tabview->init;
-    my $conf = $tabview->config;
-    $self->render_json( [ map { $_->to_json } $conf->panels ] );
-}
-
-sub show_tab {
-    my ($self) = @_;
-    my $common_name = $self->stash('common_name');
-    if ( !$self->check_organism($common_name) ) {
-        $self->render_not_found;
-        return;
-    }
-    $self->set_organism($common_name);
-
+	$self->stash('tab' => 'gene');
     if ( $self->stash('format') and $self->stash('format') eq 'json' ) {
         return $self->show_tab_json;
     }
@@ -119,55 +74,32 @@ sub show_tab {
 
 sub show_tab_html {
     my ($self)  = @_;
-    my $tab     = $self->stash('tab');
     my $gene_id = $self->stash('gene_id');
-    my $app     = $self->app;
-
-    my $db;
-    if ( $app->config->{tab}->{dynamic} eq $tab ) {
-
-        #convert gene id to its primary DDB id
-        my $trans_id = $self->gene2transid($gene_id);
-        if ( !$trans_id ) {    #do some octocat based template here
-            $app->log->error(
-                "unable to convert to transcript id for $gene_id");
-            return;
-        }
-        $db = Genome::Tabview::Page::Gene->new(
-            primary_id => $gene_id,
-            active_tab => $tab,
-            context    => $self,
-            model      => $app->modware->handler,
-            sub_id     => $trans_id
-        );
-        $app->log->debug("going through $trans_id");
-    }
-    else {
-        $db = Genome::Tabview::Page::Gene->new(
-            primary_id => $gene_id,
-            active_tab => $tab,
-            context    => $self,
-            model      => $app->modware->handler
-        );
-    }
-
-    $self->stash( $db->result );
+    my $tabview = Genome::Tabview::Page::Gene->new(
+        primary_id => $gene_id,
+        base_url   => $self->url_for('current'),
+        active_tab => $self->stash('tab')
+    );
+    $tabview->model( $self->app->modware->handler );
+    $self->stash( $tabview->result );
     $self->render( template => 'gene/index' );
-
 }
 
 sub show_tab_json {
     my ($self) = @_;
-
-    my $tab     = $self->stash('tab');
     my $gene_id = $self->stash('gene_id');
 
     my $factory = Genome::Factory::Tabview::Tab->new(
-        tab        => $tab,
         primary_id => $gene_id,
-        base_url   => $self->base_url model => ''
+        tab        => $self->stash('tab'),
+        context    => $self
     );
-    $self->render_json( $self->panel_to_json($factory) );
+
+    my $tabview = $factory->instantiate;
+    $tabview->model( $self->app->modware->handler );
+    $tabview->init;
+    my $conf = $tabview->config;
+    $self->render_json( [ map { $_->to_json } $conf->panels ] );
 }
 
 sub show_section {
@@ -184,74 +116,18 @@ sub show_section {
     $self->show_section_html;
 }
 
-sub section_html {
+
+sub show_section_json {
     my ($self) = @_;
-
-    my $gene_id = $self->stash('gene_id');
-    my $tab     = $self->stash('tab');
-    my $section = $self->stash('section');
-
-    if ( $self->app->config->{tab}->{dynamic} eq $tab ) {
-        my $db = Genome::Tabview::Page::Gene->new(
-            primary_id => $gene_id,
-            active_tab => $tab,
-            base_url   => $self->base_url,
-            sub_id     => $section,
-        );
-
-        $self->stash( $db->result );
-        $self->render( template => 'gene/index' );
-    }
-}
-
-sub section_json {
-    my ($self) = @_;
-
-    my $tab     = $self->stash('tab');
-    my $section = $self->stash('section');
-    my $gene_id = $self->stash('gene_id');
-
     my $factory;
-    if ( $self->is_ddb($section) ) {
-        $factory = Genome::Factory::Tabview::Tab->new(
-            tab        => $tab,
-            primary_id => $section,
-            base_url   => $self->base_url
-        );
-    }
-    else {
-        $factory = Genome::Factory::Tabview::Section->new(
-            base_url   => $self->base_url,
-            primary_id => $gene_id,
-            tab        => $tab,
-            section    => $section,
-            context    => $self
-        );
-    }
-    $self->render_json( $self->panel_to_json($factory) );
-}
-
-sub show_subsection_json {
-    my ($self) = @_;
-    my $common_name = $self->stash('common_name');
-    if ( !$self->check_organism($common_name) ) {
-        $self->render_not_found;
-        return;
-    }
-    $self->set_organism($common_name);
-
-    my $gene_id = $self->stash('gene_id');
-    my $tab     = $self->stash('tab');
-    my $subid = $self->stash('subid');
-    my $sub_section = $self->stash('subsection');
-
-    my $factory = Genome::Factory::Tabview::Section->new(
-        primary_id => $subid,
-        tab        => $tab,
-        section    => $sub_section,
-        context    => $self, 
-        model => $self->app->modware->handler
+    $factory = Genome::Factory::Tabview::Section->new(
+        primary_id => $self->stash('gene_id'),
+        tab        => 'protein',
+        section    => $self->stash('id'),
+        context    => $self,
+        model      => $self->app->modware->handler
     );
+    $self->render_json( $self->panel_to_json($factory) );
 }
 
 1;
