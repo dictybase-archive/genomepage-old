d26f325b90f0d1002fabefaca6de4a9e78e7f7ea
Siddhartha Basu <biosidd@gmail.com>
2012-08-03 13:47:25 -0500

    script for deleting cache

--- bin/empty_cache.pl
+++ bin/empty_cache.pl
@@ -1,106 +1,86 @@
-#!/usr/bin/perl -w
+#!/usr/bin/env perl
 
 use strict;
 use Data::Dumper;
 use Pod::Usage;
 use Getopt::Long;
 use CHI;
-use Config::Simple;
+use YAML qw/LoadFile/;
+use FindBin qw/$Bin/;
 
-my $key = 'test_CURATED_to_id';
-GetOptions( 'h|help' => sub { pod2usage(1); }, 'k|key:s' => \$key );
+my $mode = 'development';
+GetOptions( 'h|help' => sub { pod2usage(1); }, 'm|mode:s' => \$mode );
+pod2usage('no url is given') if !$ARGV[0];
+my $key = $ARGV[0];
 
-die "no config file given\n" if !$ARGV[0];
+my $yaml = "$Bin/../conf/$mode.yaml";
+die "no $yaml file found\n" if !-e $yaml;
 
-my $config = Config::Simple->new( $ARGV[0] );
+my $config = LoadFile($yaml);
+die "no cache section found\n" if not defined $config->{cache}->{options};
+my $opt = $config->{cache}->{options};
+$opt->{debug} = 1;
 
-my $cache = CHI->new(
-    driver    => $config->param('cache.driver'),
-    servers   => [ $config->param('cache.servers') ],
-    namespace => $config->param('cache.namespace'),
-);
+my $cache = CHI->new( %$opt );
 
 if ( $cache->is_valid($key) ) {
-	print Dumper $cache->get($key);
+    print "key $key exists!!!!\n";
     $cache->remove($key);
-    print "removed the key\n";
+    print "deleted cache for key $key\n";
+}
+else {
+    print "key $key not found in cache\n";
 }
 
 =head1 NAME
 
-B<Application name> - [One line description of application purpose]
+B<empty_cache> - [Removed the cached response of a given url]
 
 
 =head1 SYNOPSIS
+ 
+  perl empty_cache [-m|--mode <development|staging|..>] <url>
 
-=for author to fill in:
-Brief code example(s) here showing commonest usage(s).
-This section will be as far as many users bother reading
-so make it as educational and exeplary as possible.
+  perl empty_cache 'http://localhost:9850/purpureum'
+
+  perl empty_cache -m staging 'http://localhost:9850/purpureum'
 
 
 =head1 REQUIRED ARGUMENTS
 
-=for author to fill in:
-A complete list of every argument that must appear on the command line.
-when the application  is invoked, explaining what each of them does, any
-restrictions on where each one may appear (i.e., flags that must appear
-		before or after filenames), and how the various arguments and options
-may interact (e.g., mutual exclusions, required combinations, etc.)
-	If all of the application's arguments are optional, this section
-	may be omitted entirely.
+B<url> - Web url whose cache will be removed.
 
 
-	=head1 OPTIONS
+=head1 OPTIONS
 
-	B<[-h|-help]> - display this documentation.
+B<[-h|-help]> - display this documentation.
 
-	=for author to fill in:
-	A complete list of every available option with which the application
-	can be invoked, explaining what each does, and listing any restrictions,
-	or interactions.
-	If the application has no options, this section may be omitted entirely.
+B<[-m|--mode]> - Web application mode,  default is I<development>
 
 
-	=head1 DESCRIPTION
+=head1 CONFIGURATION AND ENVIRONMENT
 
-	=for author to fill in:
-	Write a full description of the module and its features here.
-	Use subsections (=head2, =head3) as appropriate.
+=over
 
+=item Needs to run from the root of web application
 
-	=head1 DIAGNOSTICS
+=back
 
-	=head1 CONFIGURATION AND ENVIRONMENT
+=head1 DEPENDENCIES
 
-	=head1 DEPENDENCIES
-
-	=head1 BUGS AND LIMITATIONS
-
-	=for author to fill in:
-	A list of known problems with the module, together with some
-	indication Whether they are likely to be fixed in an upcoming
-	release. Also a list of restrictions on the features the module
-	does provide: data types that cannot be handled, performance issues
-	and the circumstances in which they may arise, practical
-	limitations on the size of data sets, special cases that are not
-	(yet) handled, etc.
-
-	No bugs have been reported.Please report any bugs or feature requests to
-
-	B<Siddhartha Basu>
+None as long as the dependencies for web application are installed.
 
 
-	=head1 AUTHOR
+=head1 AUTHOR
 
-	I<Siddhartha Basu>  B<siddhartha-basu@northwestern.edu>
+I<Siddhartha Basu>  B<siddhartha-basu@northwestern.edu>
 
-	=head1 LICENCE AND COPYRIGHT
+=head1 LICENCE AND COPYRIGHT
 
-	Copyright (c) B<2010>, Siddhartha Basu C<<siddhartha-basu@northwestern.edu>>. All rights reserved.
+Copyright (c) B<2010>, Siddhartha Basu C<<siddhartha-basu@northwestern.edu>>. All rights reserved.
 
-	This module is free software; you can redistribute it and/or
-	modify it under the same terms as Perl itself. See L<perlartistic>.
+This module is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself. See L<perlartistic>.
 
 
 
